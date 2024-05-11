8e89f1ab1ce324c129a5600a1b8a4a31d6cf6022
Noah Kiser <noah.kiser@gmail.com>
2024-05-10 22:43:41 -0500

    Add video toggle button

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         adam: resolve(__dirname, "adam.html"),
+        // add future PARTNERwith sites here copying Adam
       },
     },
   },
