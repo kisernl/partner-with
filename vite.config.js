460516f5d0a503e34e5c5be0f6b05665ad88b7c5
Noah Kiser <noah.kiser@gmail.com>
2024-05-23 22:51:40 -0500

    updated vite rollup with kristin file

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         adam: resolve(__dirname, "adam/index.html"),
+        kristin: resolve(__dirname, "kristin/index.html"),
         // add future PARTNERwith sites here copying Adam
         // to make resolve(__dirname, "adam/index.html")
       },
