a3ba7c893188c45d807f690bc6d2d32910a7103a
Noah Kiser <noah.kiser@gmail.com>
2024-06-13 16:05:21 -0500

    add js for partner button and form for bloomerang

--- adam/adam.js
+++ adam/adam.js
@@ -1,3 +1,16 @@
+// smooth scroll to partner section
+document
+  .getElementById("link-to-partner")
+  .addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent the default anchor link behavior
+
+    // Use the scrollTo method with smooth behavior
+    window.scrollTo({
+      top: document.getElementById("partner-block").offsetTop, // Get the vertical position of the target element
+      behavior: "smooth", // Enable smooth scrolling
+    });
+  });
+
 // Config
 const isOpenClass = "modal-is-open";
 const openingClass = "modal-is-opening";
@@ -67,3 +80,41 @@ const getScrollbarWidth = () => {
 const isScrollbarVisible = () => {
   return document.body.scrollHeight > screen.height;
 };
+
+// javascript for buttons
+
+document
+  .querySelector("#prayer-button")
+  .addEventListener("click", prayerToggle);
+function prayerToggle() {
+  const prayerDiv = document.getElementById("prayer-div");
+  if (prayerDiv.style.display === "none") {
+    prayerDiv.style.display = "block"; // Show the div
+  } else {
+    prayerDiv.style.display = "none"; // Hide the div
+  }
+}
+
+document
+  .querySelector("#mailchimp-button")
+  .addEventListener("click", divToggle);
+function divToggle() {
+  const mailchimpDiv = document.getElementById("mailchimp-div");
+  if (mailchimpDiv.style.display === "none") {
+    mailchimpDiv.style.display = "block"; // Show the div
+  } else {
+    mailchimpDiv.style.display = "none"; // Hide the div
+  }
+}
+
+document
+  .querySelector("#support-button")
+  .addEventListener("click", supportToggle);
+function supportToggle() {
+  const supportDiv = document.getElementById("support-div");
+  if (supportDiv.style.display === "none") {
+    supportDiv.style.display = "block"; // Show the div
+  } else {
+    supportDiv.style.display = "none"; // Hide the div
+  }
+}
